3827e5314d066690637ffb26b42d6050e4ba64df
˹Sʜɪᴠᴀᴍ ꭙ Jᴀɪsᴡᴀʟ˼❤️ <111850575+Shivam892483@users.noreply.github.com>
2023-05-27 22:47:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("28564672"))
-API_HASH = getenv("89cf205dd07d743a6a869817e559d503")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("1983049881"))
+OWNER_ID = int(getenv("OWNER_ID"))
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/4158367c80a24a0cce80f.jpg")
