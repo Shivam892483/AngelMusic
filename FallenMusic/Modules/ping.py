295496811a17cd0fdac6ab20a92230b4bb595479
˹Sʜɪᴠᴀᴍ ꭙ Jᴀɪsᴡᴀʟ˼❤️ <111850575+Shivam892483@users.noreply.github.com>
2023-05-27 22:04:15 +0530

    Update ping.py

--- FallenMusic/Modules/ping.py
+++ FallenMusic/Modules/ping.py
@@ -60,7 +60,7 @@ async def ping_fallen(_, message: Message):
                     InlineKeyboardButton("❄ sᴜᴘᴘᴏʀᴛ ❄", url=config.SUPPORT_CHAT),
                     InlineKeyboardButton(
                         "✨ sᴏᴜʀᴄᴇ ✨",
-                        url="https://github.com/AnonymousX1025/FallenMusic",
+                        url="https://te.legra.ph/file/d3cc723e62087d51b19f0.jpg",
                     ),
                 ],
             ]
